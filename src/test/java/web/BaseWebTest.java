57d2f21975e36127dda449e2d415e33fad6d034c
ihor-kucheriavyi21 <kucherjavyj228@gmail.com>
2024-07-31 22:58:42 +0300

    driver changes

--- src/test/java/web/BaseWebTest.java
+++ src/test/java/web/BaseWebTest.java
@@ -4,6 +4,7 @@ import static com.codeborne.selenide.Selenide.open;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.BeforeClass;
 
 public abstract class BaseWebTest {
@@ -11,7 +12,12 @@ public abstract class BaseWebTest {
   @BeforeClass
   public void openSession() {
     System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
-    WebDriver driver = new ChromeDriver();
+    ChromeOptions options = new ChromeOptions();
+    options.addArguments("--headless");
+    options.addArguments("--no-sandbox");
+    options.addArguments("--disable-dev-shm-usage");
+    options.addArguments("--disable-gpu");
+    WebDriver driver = new ChromeDriver(options);
     open("https://www.google.com/");
   }
 }
