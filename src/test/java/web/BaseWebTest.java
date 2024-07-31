595ea700515390826af42b270591751a29b81424
ihor-kucheriavyi21 <kucherjavyj228@gmail.com>
2024-07-31 22:54:06 +0300

    driver changes

--- src/test/java/web/BaseWebTest.java
+++ src/test/java/web/BaseWebTest.java
@@ -2,12 +2,16 @@ package web;
 
 import static com.codeborne.selenide.Selenide.open;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeClass;
 
 public abstract class BaseWebTest {
 
   @BeforeClass
   public void openSession() {
+    System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
+    WebDriver driver = new ChromeDriver();
     open("https://www.google.com/");
   }
 }
