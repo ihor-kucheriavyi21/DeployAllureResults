969b306990b6e0517438361cb3fcb1a144be5970
ihor-kucheriavyi21 <kucherjavyj228@gmail.com>
2024-08-05 17:25:22 +0300

    added option

--- src/test/java/web/BaseWebTest.java
+++ src/test/java/web/BaseWebTest.java
@@ -11,11 +11,13 @@ public abstract class BaseWebTest {
 
   @BeforeClass
   public void openSession() {
+    System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
     ChromeOptions options = new ChromeOptions();
     options.addArguments("--headless");
     options.addArguments("--no-sandbox");
     options.addArguments("--disable-dev-shm-usage");
     options.addArguments("--disable-gpu");
+    options.addArguments("--remote-debugging-pipe");
     WebDriver driver = new ChromeDriver(options);
     open("https://www.google.com/");
   }
