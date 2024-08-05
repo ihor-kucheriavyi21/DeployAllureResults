a7b65ec5c073917738572318a3ed9906bd427d35
ihor-kucheriavyi21 <kucherjavyj228@gmail.com>
2024-08-05 17:10:56 +0300

    lt options

--- src/test/java/web/BaseWebTest.java
+++ src/test/java/web/BaseWebTest.java
@@ -2,7 +2,6 @@ package web;
 
 import static com.codeborne.selenide.Selenide.open;
 
-import java.util.HashMap;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -12,16 +11,12 @@ public abstract class BaseWebTest {
 
   @BeforeClass
   public void openSession() {
-    ChromeOptions browserOptions = new ChromeOptions();
-    browserOptions.setPlatformName("Windows 10");
-    browserOptions.setBrowserVersion("126");
-    HashMap<String, Object> ltOptions = new HashMap<String, Object>();
-    ltOptions.put("username", "ikdevexpert");
-    ltOptions.put("accessKey", "Mja7jOlFWCGew4vn3q7hwukPGFUTOBDSXB4kIUpNuwSgmfrrW1");
-    ltOptions.put("project", "Untitled");
-    ltOptions.put("selenium_version", "4.0.0");
-    ltOptions.put("w3c", true);
-    browserOptions.setCapability("LT:Options", ltOptions);
+    ChromeOptions options = new ChromeOptions();
+    options.addArguments("--headless");
+    options.addArguments("--no-sandbox");
+    options.addArguments("--disable-dev-shm-usage");
+    options.addArguments("--disable-gpu");
+    WebDriver driver = new ChromeDriver(options);
     open("https://www.google.com/");
   }
 }
