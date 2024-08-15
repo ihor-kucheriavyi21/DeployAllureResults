a9473e57b7293805fd77ba5d333b6cdab469426f
ihor-kucheriavyi21 <kucherjavyj228@gmail.com>
2024-08-15 16:18:19 +0300

    changed chrome to firefox

--- src/test/java/web/BaseWebTest.java
+++ src/test/java/web/BaseWebTest.java
@@ -3,6 +3,7 @@ package web;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.BeforeClass;
 
 public abstract class BaseWebTest {
@@ -11,13 +12,16 @@ public abstract class BaseWebTest {
 
   @BeforeClass
   public void openSession() {
-    System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
+  /*  System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
     ChromeOptions options = new ChromeOptions();
     options.addArguments("--no-sandbox");
     options.addArguments("--disable-dev-shm-usage");
     options.addArguments("--disable-gpu");
     options.addArguments("--remote-debugging-pipe");
-    driver = new ChromeDriver(options);
+    driver = new ChromeDriver(options);*/
+    // Set the path to the Geckodriver
+    // Initialize the FirefoxDriver
+    driver = new FirefoxDriver();
     driver.navigate().to("https://www.google.com/");
   }
 }
